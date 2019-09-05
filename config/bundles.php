f4c70f31d517af101d395bdb71381a262c722851
Grégoire Pineau <lyrixx@lyrixx.info>
2019-09-05 17:45:22 +0200

    Added monolog

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
