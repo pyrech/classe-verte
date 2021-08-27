af064bb10b58ed95f63efcac24e2b9b01331f186
Loïck Piera <pyrech@gmail.com>
2021-08-27 09:21:23 +0200

    Update deps

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
